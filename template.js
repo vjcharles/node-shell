039984ff694b98a26ad9ef579bdb542b18997fee
V <vjcharles@gmail.com>
2019-03-21 01:08:43 -0400

    Update template.js

--- template.js
+++ template.js
@@ -5,11 +5,11 @@ var argv = require('minimist')(process.argv.slice(2));
 //rename as needed.
 function main(argv) {
   //parse args
-
+  
   //do stuff
 
   //display output
-  console.log("hello, template.");
+  console.log(argv);
 }
 
 main(argv);
