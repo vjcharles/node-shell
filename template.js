8d5373bba09eb7e20cd5318790aff6b3c8657579
V <vjcharles@gmail.com>
2019-03-21 00:46:16 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node 
+ 
+var argv = require('minimist')(process.argv.slice(2)); 
+
+//rename as needed.
+function main(argv) {
+  //parse args
+
+  //do stuff
+
+  //display output
+  console.log("hello, template.");
+}
+
+main(argv);
